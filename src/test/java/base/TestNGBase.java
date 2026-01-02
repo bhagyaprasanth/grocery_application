88d50ee4d23050edb36fb622a50d2f20a03d0686
bhagya <bhagyaprasanth@gmail.com>
2026-01-02 08:39:27 +0530

    Day - 7 :: ScreenshotUtility, ExtentReportUtility, Listener

--- src/test/java/base/TestNGBase.java
+++ src/test/java/base/TestNGBase.java
@@ -1,9 +1,14 @@
 package base;
 
+import java.io.IOException;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import utilities.ScreenshotUtility;
 
 public class TestNGBase {
 
@@ -19,5 +24,16 @@ public class TestNGBase {
 	@AfterMethod
 	public void closeBrowser() {
 		driver.close();
+	}
+	@AfterMethod(alwaysRun = true)
+	public void driverQuit(ITestResult iTestResult) throws IOException
+	{
+		if(iTestResult.getStatus()==ITestResult.FAILURE)
+		{
+			ScreenshotUtility screenShot=new ScreenshotUtility();
+			screenShot.getScreenshot(driver, iTestResult.getName());
+		}
+		driver.quit();
+
 	}
 }
